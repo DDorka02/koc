afc8bfc90c661042057be4d131ff0df4dd623c03
DDorka02 <dobszaydorka@gmail.com>
2024-04-15 10:16:28 +0200

    hatlom

---
+++ src/main/Jatek.java
@@ -0,0 +1,18 @@
+package main;
+
+
+public class Jatek {
+    //konstruktor
+    
+    public Jatek() {
+        start();
+    }
+    
+    void start(){
+        System.out.println("Gondoltam egy számra 0 és 9 között.");
+    }
+    
+    
+
+    
+}
---
+++ src/main/JatekIndito.java
@@ -0,0 +1,15 @@
+package main;
+
+
+public class JatekIndito {
+
+
+    public static void main(String[] args) {
+//        new Jatek().start();
+
+//        Jatek referencia = new Jatek();
+//        referencia.start();
+        
+        new Jatek();
+    }
+}
